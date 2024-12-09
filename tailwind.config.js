51a6fb7b484aea7bd611ecc5c5f3ee63441c6869
Avaz <avazbekrozimboyev08@gmail.com>
2024-12-10 02:09:09 +0500

    3-oy 11-dars

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      white: "#fff",
+      a47: " #a0a747",
+      a44: "#773844",
+      a57: "#1f2057",
+      a87: "#465587",
+      e9: " #77dfe9",
+      b2: "#d6d1b2",
+      a53: " #d5b753",
+      a33: " #be6833",
+      a1a: "#1a1a1a",
+      a62: "#646562",
+      d9: "#eae3d9",
+      b6: "#c2bdb6",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
